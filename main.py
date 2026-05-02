963caa4f7f006af5dbc9f2208d7b6fdf7bdde6df
SunJong2 <tjsqhd9790@gmail.com>
2026-05-02 15:59:41 +0900

    Fast API 날씨 서버 구현

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+import requests
+import json
+
+app = FastAPI()
+
+@app.get("/weather/{city}")
+def get_weather(city: str):
+    response = requests.get(f"https://wttr.in/{city}?format=j1")
+    data = response.json()
+    current = data["current_condition"][0]
+    
+    result = {
+        "city": city,
+        "desc": current["weatherDesc"][0]["value"],
+        "temp": current["temp_C"],
+        "feels_like": current["FeelsLikeC"],
+        "humidity": current["humidity"],
+        "pressure": current["pressure"],
+        "visibility": current["visibility"]
+    }
+    return JSONResponse(content=result, media_type="application/json; charset=utf-8")
